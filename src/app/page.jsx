9e8cb6778bb96558a6d149b948f219cd951cbc85
SebastianOG509 <seortizg@unal.edu.co>
2025-07-30 16:07:32 -0500

    First commit

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+  return (
+    <main>
+      <h1>Aqui estara el sitio principal del Sistema de Gestion de Visitas</h1>
+      <button>Login</button>
+    </main>
+  );
+} 
